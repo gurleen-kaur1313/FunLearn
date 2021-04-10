fb4275ae75b7bb4b219d221a5f66a5e6412b8cfb
Harmanjit Singh <harmanjit140500@gmail.com>
2021-04-11 01:28:34 +0530

    mutation

---
+++ android-app/funlearn/lib/server/upload.dart
@@ -0,0 +1,38 @@
+import 'package:funlearn/server/login_gql.dart';
+import "package:graphql/client.dart";
+
+Future<bool> getTest() async {
+  HttpLink _httpLink = HttpLink(
+    'https://funlearn.herokuapp.com/graphql/',
+  );
+
+  AuthLink _authLink = AuthLink(
+    getToken: () async => 'JWT $token',
+  );
+
+  Link _link = _authLink.concat(_httpLink);
+  GraphQLClient client = GraphQLClient(
+    cache: GraphQLCache(),
+    link: _link,
+  );
+
+  String queryString = """
+mutation{
+  updateAssignment(id:"" , url:""){
+    __typename
+  }
+}
+""";
+
+  MutationOptions options = MutationOptions(
+    document: gql(queryString),
+  );
+
+  QueryResult data = await client.mutate(options);
+  if (data.hasException) {
+    print(data.exception.toString());
+    return false;
+  }
+
+  return true;
+}
