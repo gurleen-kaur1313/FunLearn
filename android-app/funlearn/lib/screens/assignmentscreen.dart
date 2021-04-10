96bfb8031a1a4a01cbf98bfa8b4ac9410b236f30
Harmanjit Singh <harmanjit140500@gmail.com>
2021-04-11 00:58:46 +0530

    file upload

--- android-app/funlearn/lib/screens/assignmentscreen.dart
+++ android-app/funlearn/lib/screens/assignmentscreen.dart
@@ -1,18 +1,30 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:funlearn/constants/text.dart';
 import 'package:funlearn/model/assignments.dart';
 import 'package:file_picker/file_picker.dart';
 
-class MyAssignment extends StatelessWidget {
+class MyAssignment extends StatefulWidget {
   final Assignments obj;
+
   MyAssignment({Key key, this.obj}) : super(key: key);
+
+  @override
+  _MyAssignmentState createState() => _MyAssignmentState();
+}
+
+class _MyAssignmentState extends State<MyAssignment> {
+  final storage = FirebaseStorage.instance;
+  bool clicked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: BoldText(text: obj.name),
+        title: BoldText(text: widget.obj.name),
       ),
       body: Column(
         mainAxisSize: MainAxisSize.max,
@@ -21,24 +33,25 @@ class MyAssignment extends StatelessWidget {
         children: [
           Container(
             margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
-            child: BoldText(text: "Subject : ${obj.subject}", size: 30),
+            child: BoldText(text: "Subject : ${widget.obj.subject}", size: 30),
           ),
           Container(
             margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
             child: RegularText(
-                text: "Maximum marks : ${obj.totalmarks}", size: 25),
+                text: "Maximum marks : ${widget.obj.totalmarks}", size: 25),
           ),
           Container(
             margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
             child: RegularText(
-                text: "Duedate : ${obj.duedate.substring(0, 10)}", size: 23),
+                text: "Duedate : ${widget.obj.duedate.substring(0, 10)}",
+                size: 23),
           ),
           Divider(),
           Container(
             margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: BoldText(text: "Submitted : ${obj.ontime}", size: 16),
+            child: BoldText(text: "Submitted : ${widget.obj.ontime}", size: 16),
           ),
-          (obj.ontime == "Y")
+          (widget.obj.ontime == "Y")
               ? Container(
                   margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
                   child: TextButton(
@@ -60,14 +73,26 @@ class MyAssignment extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-                  child: BoldText(text:(obj.ontime=="Y")? "Update : ":"Submit :", size: 20),
+                  child: BoldText(
+                      text:
+                          (widget.obj.ontime == "Y") ? "Update : " : "Submit :",
+                      size: 20),
                 ),
                 TextButton(
                     style: ButtonStyle(
                       backgroundColor:
                           MaterialStateProperty.all<Color>(Colors.cyan),
                     ),
-                    onPressed: () {},
+                    onPressed: () async {
+                      setState(() {
+                        clicked = true;
+                      });
+                      FilePickerResult result =
+                          await FilePicker.platform.pickFiles();
+                      if (result != null) {
+                        File file = File(result.files.single.path);
+                      } else {}
+                    },
                     child: BoldText(
                       text: "Upload file",
                       color: Colors.black,
@@ -76,6 +101,12 @@ class MyAssignment extends StatelessWidget {
               ],
             ),
           ),
+          (clicked)
+              ? Container(
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: BoldText(text: "File : ", size: 20),
+                )
+              : SizedBox(),
         ],
       ),
     );
