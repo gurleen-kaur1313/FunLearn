4f9ac0906033af3ec04682d957198de9960308a8
Harmanjit Singh <harmanjit140500@gmail.com>
2021-04-11 17:51:38 +0530

    mutation created

--- android-app/funlearn/lib/server/upload.dart
+++ android-app/funlearn/lib/server/upload.dart
@@ -1,38 +1,38 @@
-// import 'package:funlearn/server/login_gql.dart';
-// import "package:graphql/client.dart";
+import 'package:funlearn/server/login_gql.dart';
+import "package:graphql/client.dart";
 
-// Future<bool> uploadAssignment(String id, String url) async {
-//   HttpLink _httpLink = HttpLink(
-//     'https://funlearn.herokuapp.com/graphql/',
-//   );
+Future<bool> uploadAssignment(String id, String url) async {
+  HttpLink _httpLink = HttpLink(
+    'https://funlearn.herokuapp.com/graphql/',
+  );
 
-//   AuthLink _authLink = AuthLink(
-//     getToken: () async => 'JWT $token',
-//   );
+  AuthLink _authLink = AuthLink(
+    getToken: () async => 'JWT $token',
+  );
 
-//   Link _link = _authLink.concat(_httpLink);
-//   GraphQLClient client = GraphQLClient(
-//     cache: GraphQLCache(),
-//     link: _link,
-//   );
+  Link _link = _authLink.concat(_httpLink);
+  GraphQLClient client = GraphQLClient(
+    cache: GraphQLCache(),
+    link: _link,
+  );
 
-//   String queryString = """
-// mutation{
-//   updateAssignment(id: "$id" , url: "$url"){
-//     __typename
-//   }
-// }
-// """;
+  String queryString = """
+mutation{
+  updateAssignment(id: "$id" , url: "$url"){
+    __typename
+  }
+}
+""";
 
-//   MutationOptions options = MutationOptions(
-//     document: gql(queryString),
-//   );
+  MutationOptions options = MutationOptions(
+    document: gql(queryString),
+  );
 
-//   QueryResult data = await client.mutate(options);
-//   if (data.hasException) {
-//     print(data.exception.toString());
-//     return false;
-//   }
+  QueryResult data = await client.mutate(options);
+  if (data.hasException) {
+    print(data.exception.toString());
+    return false;
+  }
 
-//   return true;
-// }
+  return true;
+}
